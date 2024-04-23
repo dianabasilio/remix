5db9f92952a80d79e6d730b49f0a171f6086bc50
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio.GPODEACERO.CORP>
2024-04-23 11:08:25 -0600

    Creating contacts.

--- app/root.tsx
+++ app/root.tsx
@@ -11,9 +11,14 @@ import {
 } from "@remix-run/react";
 import { LinksFunction } from "@remix-run/node";
 
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
 import appStylesHref from "./app.css?url";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
